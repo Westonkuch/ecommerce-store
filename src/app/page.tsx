b1955cbed0a56dc7c9822187072e3ce4960b747f
Westonkuch <150453909+Westonkuch@users.noreply.github.com>
2025-05-13 10:56:57 -0600

    Update page.tsx
    updated hub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-white"
-          href="https://github.com/"
+          href="https://github.com/Westonkuch/ecommerce-store"
           target="_blank"
           rel="noopener noreferrer"
         >
